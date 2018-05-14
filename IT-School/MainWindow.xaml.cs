3baa2228fb2b92c51fa71ea67afc9c5dfd6b825a
Александр Полторацкий <pag.2010@ya.ru>
2018-05-15 00:04:33 +0300

    Добавил скачку файла по кнопке и считывание элемента Excel
    Добавил скачку файла по кнопке и считывание элемента Excel. Создал класс для дальнейшего изменения

--- IT-School/MainWindow.xaml.cs
+++ IT-School/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace IT_School
 {
@@ -20,11 +25,65 @@ namespace IT_School
     /// </summary>
     public partial class MainWindow : Window
     {
-        
+
+        public class School
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+
+        ObservableCollection<School> custdata = new ObservableCollection<School>();
+
         public MainWindow()
         {
             InitializeComponent();
-            gridProducts.ItemsSource = "data.xlm";
+
+            //GetData() creates a collection of Customer data from a database
+            
+
+            School a = new School();
+            a.FirstName = "Sasha";
+            a.LastName = "Sasha";
+            custdata.Add(a);
+            //Bind the DataGrid to the customer data
+            DG1.DataContext = custdata;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string filename = "data.xls";
+            string adress = "http://www.admoblkaluga.ru/opendata/4027064263-ObrazovanieReestrUchrejd/data-124-structure-1.xls";
+            WebClient myWebClient = new WebClient();
+            myWebClient.DownloadFile(adress, filename);
+        }
+        private static string filename = Directory.GetCurrentDirectory()+@"\data.xls";
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            
+            Microsoft.Office.Interop.Excel.Application ObjExcel = new Microsoft.Office.Interop.Excel.Application();
+            //Открываем книгу.                                                                                                                                                        
+            Microsoft.Office.Interop.Excel.Workbook ObjWorkBook = ObjExcel.Workbooks.Open(filename, 0, true, 5, "", "", false, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "", true, false, 0, true, false, false);
+            //Выбираем таблицу(лист).
+            Microsoft.Office.Interop.Excel.Worksheet ObjWorkSheet;
+            ObjWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)ObjWorkBook.Sheets[1];
+            string infoA = "";
+            string infoB = "";
+            //Выбираем первые сто записей из столбца.
+            //Выбираем область таблицы. (в нашем случае просто ячейку)
+            Microsoft.Office.Interop.Excel.Range rangeA = ObjWorkSheet.get_Range("A1");
+            Microsoft.Office.Interop.Excel.Range rangeB = ObjWorkSheet.get_Range("B1");
+            //Добавляем полученный из ячейки текст.
+            infoA = rangeA.Text.ToString();
+            infoB = rangeB.Text.ToString();
+            School a = new School();
+            a.FirstName = infoA;
+            a.LastName = infoB;
+            custdata.Add(a);
         }
     }
 }
