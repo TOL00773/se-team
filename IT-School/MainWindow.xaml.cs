70c799d47748dc7db8e48343d6af9be25017aa37
Александр Полторацкий <pag.2010@ya.ru>
2018-05-16 00:17:41 +0300

    Полурабочий проект без фильтров

--- IT-School/MainWindow.xaml.cs
+++ IT-School/MainWindow.xaml.cs
@@ -70,17 +70,57 @@ namespace IT_School
             string info = "";
             //Выбираем первые сто записей из столбца.
             //Выбираем область таблицы. (в нашем случае просто ячейку)
+            object[,] arr = ObjWorkBook.get_Range("A1:B2000").Value;
 
-            
-            for (int i = 1; i < 101; i++)
+            for (int i = 1; i < 201; i++)
             {
-                //Выбираем область таблицы. (в нашем случае просто ячейку)
-                Excel.Range range = ObjWorkSheet.get_Range("B" + i.ToString(), "B" + i.ToString());
-                //Добавляем полученный из ячейки текст.
-                info = range.Text.ToString();
                 Organization a = new Organization();
+                Excel.Range rangeA = ObjWorkSheet.get_Range("A" + i.ToString(), "A" + i.ToString());
+                info = rangeA.Text.ToString();
                 a.AccName = info;
+                Excel.Range range = ObjWorkSheet.get_Range("B" + i.ToString(), "B" + i.ToString());
+                info = range.Text.ToString();
                 a.Name = info;
+                Excel.Range rangeC = ObjWorkSheet.get_Range("C" + i.ToString(), "C" + i.ToString());
+                info = rangeC.Text.ToString();
+                a.Adress = info;
+                Excel.Range rangeD = ObjWorkSheet.get_Range("D" + i.ToString(), "D" + i.ToString());
+                info = rangeD.Text.ToString();
+                a.GeoData = info;
+                Excel.Range rangeE = ObjWorkSheet.get_Range("E" + i.ToString(), "E" + i.ToString());
+                info = rangeE.Text.ToString();
+                a.WorkTime = info;
+                Excel.Range rangeF = ObjWorkSheet.get_Range("F" + i.ToString(), "F" + i.ToString());
+                info = rangeF.Text.ToString();
+                a.GosID = info;
+                Excel.Range rangeG = ObjWorkSheet.get_Range("G" + i.ToString(), "G" + i.ToString());
+                info = rangeG.Text.ToString();
+                a.Inn = info;
+                Excel.Range rangeH = ObjWorkSheet.get_Range("H" + i.ToString(), "H" + i.ToString());
+                info = rangeH.Text.ToString();
+                a.DateBegin = info;
+                Excel.Range rangeI = ObjWorkSheet.get_Range("I" + i.ToString(), "I" + i.ToString());
+                info = rangeI.Text.ToString();
+                a.GosAccReq = info;
+                Excel.Range rangeJ = ObjWorkSheet.get_Range("J" + i.ToString(), "J" + i.ToString());
+                info = rangeJ.Text.ToString();
+                a.DateExpire = info;
+                Excel.Range rangeK = ObjWorkSheet.get_Range("K" + i.ToString(), "K" + i.ToString());
+                info = rangeK.Text.ToString();
+                a.EduSpecs = info;
+                Excel.Range rangeL = ObjWorkSheet.get_Range("L" + i.ToString(), "L" + i.ToString());
+                info = rangeL.Text.ToString();
+                a.ReMake = info;
+                Excel.Range rangeM = ObjWorkSheet.get_Range("M" + i.ToString(), "M" + i.ToString());
+                info = rangeM.Text.ToString();
+                a.StopStart = info;
+                Excel.Range rangeN = ObjWorkSheet.get_Range("N" + i.ToString(), "N" + i.ToString());
+                info = rangeN.Text.ToString();
+                a.StopExec = info;
+                Excel.Range rangeO = ObjWorkSheet.get_Range("O" + i.ToString(), "O" + i.ToString());
+                info = rangeO.Text.ToString();
+                a.Stop = info;
+
                 custdata.Add(a);
 
             }
--- IT-School/Organisation.cs
+++ IT-School/Organisation.cs
@@ -10,18 +10,18 @@ namespace IT_School
     {
         public string AccName { get; set; } //Наименование аккредитационного органа
         public string Name { get; set; } //Полное наименование, местонахождение организации
-        //public string Adress { get; set; } //Местонахождение юридического лица
-        //public string GeoData { get; set; } //Геоданные
-        //public string WorkTime { get; set; } //Режим работы
-        //public string GosID { get; set; } //Государственный регистрационный номер записи о создании юридического лица
-        //public string Inn { get; set; } //Идентификационный номер налогоплательщика-организации
-        //public string DateBegin { get; set; } //Дата принятия решения о государственной аккредитации
-        //public string GosAccReq { get; set; } //Реквизиты свидетельств
-        //public string DateExpire { get; set; } //Срок окончания свидетельства
-        //public List<string> EduSpecs { get; set; } //Перечень аккредитованных образовательных программ, укрупненных групп направлений подготовки и специальностей
-        //public string ReMake { get; set; } //Основание и дата переоформления свидетельства о государственной аккредитации
-        //public string StopStart { get; set; } //Основание и даты приостановлении и  возобновлении действия свидетельства о государственной аккредитации
-        //public string StopExec { get; set; } //Основание и дата лишения свидетельства о государственной аккредитации 
-        //public string Stop { get; set; } //Основание и дата прекращения действия  свидетельства о государственной аккредитации 
+        public string Adress { get; set; } //Местонахождение юридического лица
+        public string GeoData { get; set; } //Геоданные
+        public string WorkTime { get; set; } //Режим работы
+        public string GosID { get; set; } //Государственный регистрационный номер записи о создании юридического лица
+        public string Inn { get; set; } //Идентификационный номер налогоплательщика-организации
+        public string DateBegin { get; set; } //Дата принятия решения о государственной аккредитации
+        public string GosAccReq { get; set; } //Реквизиты свидетельств
+        public string DateExpire { get; set; } //Срок окончания свидетельства
+        public string EduSpecs { get; set; } //Перечень аккредитованных образовательных программ, укрупненных групп направлений подготовки и специальностей
+        public string ReMake { get; set; } //Основание и дата переоформления свидетельства о государственной аккредитации
+        public string StopStart { get; set; } //Основание и даты приостановлении и  возобновлении действия свидетельства о государственной аккредитации
+        public string StopExec { get; set; } //Основание и дата лишения свидетельства о государственной аккредитации 
+        public string Stop { get; set; } //Основание и дата прекращения действия  свидетельства о государственной аккредитации 
     }
 }
